e40a887c25963ac52f5a94341543632aec50f66d
Nugg7 <steven.guevarra.1@gmail.com>
2024-06-19 23:00:50 +0200

    changed font size and indenting in some files

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,21 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			source = {
-				null_ls.builtins.formatting.stylua, --lua
-				null_ls.builtins.formatting.prettier, -- js
-				null_ls.builtins.formatting.csharpier, --c#
-				null_ls.builtins.formatting.biome, --java/json
-				null_ls.builtins.formatting.isort, --python
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      source = {
+        null_ls.builtins.formatting.stylua, --lua
+        null_ls.builtins.formatting.prettier, -- js
+        null_ls.builtins.formatting.csharpier, --c#
+        null_ls.builtins.formatting.biome, --java/json
+        null_ls.builtins.formatting.isort, --python
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.biome,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.trivy,
         null_ls.builtins.diagnostics.pylint
-			}
-		})
-		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-	end
+      }
+    })
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+  end
 }
