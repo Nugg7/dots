92a292ed4a69efe2a0dfea8d9fd8d356829084b5
Nugg7 <steven.guevarra.1@gmail.com>
2024-06-20 00:03:52 +0200

    added keybinding for refactoring variables

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,66 +1,67 @@
-return{
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "html", "markdown_oxide", "basedpyright", "jsonls", "jdtls", "quick_lint_js", "omnisharp", "csharp_ls", "clangd", "cssls", "biome", "eslint", "tsserver"}
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities
-			}) --lua
-			lspconfig.csharp_ls.setup({
-				capabilities = capabilities
-			}) --c#
-			lspconfig.clangd.setup({
-				capabilities = capabilities
-			}) --c++ and c
-			lspconfig.html.setup({
-				capabilities = capabilities
-			}) --html
-			lspconfig.cssls.setup({
-				capabilities = capabilities
-			}) --css
-			lspconfig.markdown_oxide.setup({
-				capabilities = capabilities
-			}) --markdown
-			lspconfig.basedpyright.setup({
-				capabilities = capabilities
-			}) --python
-			lspconfig.jsonls.setup({
-				capabilities = capabilities
-			}) --json
-			lspconfig.jdtls.setup({
-				capabilities = capabilities
-			}) --java
-			lspconfig.biome.setup({
-				capabilities = capabilities
-			}) --javascript
-			lspconfig.eslint.setup({
-				capabilities = capabilities
-			}) --javascript
-			lspconfig.tsserver.setup({
-				capabilities = capabilities
-			}) --typescript
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "html", "markdown_oxide", "basedpyright", "jsonls", "jdtls", "quick_lint_js", "omnisharp", "csharp_ls", "clangd", "cssls", "biome", "eslint", "tsserver" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      }) --lua
+      lspconfig.csharp_ls.setup({
+        capabilities = capabilities
+      }) --c#
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      }) --c++ and c
+      lspconfig.html.setup({
+        capabilities = capabilities
+      }) --html
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      }) --css
+      lspconfig.markdown_oxide.setup({
+        capabilities = capabilities
+      }) --markdown
+      lspconfig.basedpyright.setup({
+        capabilities = capabilities
+      }) --python
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      }) --json
+      lspconfig.jdtls.setup({
+        capabilities = capabilities
+      }) --java
+      lspconfig.biome.setup({
+        capabilities = capabilities
+      }) --javascript
+      lspconfig.eslint.setup({
+        capabilities = capabilities
+      }) --javascript
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      }) --typescript
       lspconfig.omnisharp.setup({
-				capabilities = capabilities
-			}) -- c# 
+        capabilities = capabilities
+      }) -- c#
 
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-		end
-	}
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    end
+  }
 }
